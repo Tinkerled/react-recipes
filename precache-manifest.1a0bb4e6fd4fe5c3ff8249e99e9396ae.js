a37fb860bfac266e6794edc0b4c633dab6d3f6fc
Lucas <i.lucas.guillemette@gmail.com>
2019-12-20 22:21:22 -0500

    Updates

---
+++ precache-manifest.1a0bb4e6fd4fe5c3ff8249e99e9396ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad24c0b9d1bc3021e1ee3f5bf7755a4",
+    "url": "/react-recipes/index.html"
+  },
+  {
+    "revision": "4a76cf195366294870f9",
+    "url": "/react-recipes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2aef021cf017a5df3862",
+    "url": "/react-recipes/static/js/2.e89abdfb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-recipes/static/js/2.e89abdfb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a76cf195366294870f9",
+    "url": "/react-recipes/static/js/main.e4abf62c.chunk.js"
+  },
+  {
+    "revision": "ecf67b268f0b78ce4cf5",
+    "url": "/react-recipes/static/js/runtime-main.255dcf13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-recipes/static/media/logo.5d5d9eef.svg"
+  }
+]);
